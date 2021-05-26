fb1c2e9dd492e34aa147456ab6e0aeea2a341194
nicobabot <nico.babotaltisen@gmail.com>
2021-05-26 12:26:09 +0200

    ExerciceBehaviour interface created

--- GuessTheNumberTest/Assets/Scripts/Exercice/ExerciceGame.cs
+++ GuessTheNumberTest/Assets/Scripts/Exercice/ExerciceGame.cs
@@ -4,64 +4,21 @@ using TMPro;
 
 public class ExerciceGame : MonoBehaviour
 {
-    [Header("Exercice Data")]
-    [SerializeField] private int _maxChoices;
-    [SerializeField] private int _minRandomRange;
-    [SerializeField] private int _maxRandomRange;
-
-    [Header("Transitions Data")]
-    [SerializeField] private float _timeShowingExerciceNumber = 2;
-    [SerializeField] private float _exerciceNumberAlphaDuration= 2;
-    [SerializeField] private float _timeShowingAnswer;
-
-    [Header("Exercice Objects")]
-    [SerializeField] private TextMeshProUGUI _numberWord;
-    [SerializeField] private OptionsController _optionsController;
-    [SerializeField] private ExerciceAnswer _exerciceAnswer;
+    [SerializeField] private GameObject _exerciceObject;
 
     [Header("Text Data")]
     [SerializeField] private string _textDocument;
 
     private ExerciceGenerator _exerciceGenerator;
     private TextResourcesManager _textManager;
+    private IExerciceBehaviour _exercice;
 
     void Start()
     {
         _textManager = new TextResourcesManager(_textDocument);
         _exerciceGenerator = new ExerciceGenerator();
-        _optionsController.CreateOptions(_maxChoices);
-        _numberWord.alpha = 0;
-        StartCoroutine(PlayExercice());
-    }
-
-    private IEnumerator PlayExercice()
-    {
-        Exercice exerciceData = _exerciceGenerator.GenerateNewExercice(_maxChoices, _minRandomRange, _maxRandomRange);
-        Debug.Log(exerciceData.correctNumber);
-        _numberWord.text = _textManager.NumberToWords(exerciceData.correctNumber);
-
-        yield return ShowExerciceNumber();
-
-        yield return ChooseOptionRoutine(exerciceData);
-
-        StartCoroutine(PlayExercice());
-    }
-
-    private IEnumerator ShowExerciceNumber()
-    {
-        yield return _numberWord.DoAlphaTransition(1, _exerciceNumberAlphaDuration);
-        yield return new WaitForSeconds(_timeShowingExerciceNumber);
-        yield return _numberWord.DoAlphaTransition(0, _exerciceNumberAlphaDuration);
-    }
-
-    private IEnumerator ChooseOptionRoutine(Exercice exerciceData)
-    {
-        _optionsController.StartExercice(exerciceData, _exerciceAnswer);
-        _exerciceAnswer.StartExerciceAnswer(exerciceData, _optionsController);
-
-        yield return new WaitUntil(() => _exerciceAnswer.HasEndExercice);
-        yield return new WaitForSeconds(_timeShowingAnswer);
-
-        yield return _optionsController.EndExercice();
+        _exercice = _exerciceObject.GetComponent<IExerciceBehaviour>();
+        _exercice.Initialize(_exerciceGenerator, _textManager);
+        StartCoroutine(_exercice.Play());
     }
 }
---
+++ GuessTheNumberTest/Assets/Scripts/Exercice/Types/RecognizeNumberExercice.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class RecognizeNumberExercice : MonoBehaviour, IExerciceBehaviour
+{
+    [Header("Exercice Data")]
+    [SerializeField] private int _maxChoices;
+    [SerializeField] private int _minRandomRange;
+    [SerializeField] private int _maxRandomRange;
+    
+    [Header("Transitions Data")]
+    [SerializeField] private float _timeShowingExerciceNumber = 2;
+    [SerializeField] private float _exerciceNumberAlphaDuration= 2;
+    [SerializeField] private float _timeShowingAnswer;
+    
+    [Header("Exercice Objects")]
+    [SerializeField] private TextMeshProUGUI _numberWord;
+    [SerializeField] private OptionsController _optionsController;
+    [SerializeField] private ExerciceAnswer _exerciceAnswer;
+
+    private ExerciceGenerator _exerciceGenerator;
+    private TextResourcesManager _textManager;
+    
+    public void Initialize(ExerciceGenerator exerciceGenerator, TextResourcesManager textManager)
+    {
+        _exerciceGenerator = exerciceGenerator;
+        _textManager = textManager;
+        _optionsController.CreateOptions(_maxChoices);
+        _numberWord.alpha = 0;
+    }
+
+    public IEnumerator Play()
+    {
+        Exercice exerciceData = _exerciceGenerator.GenerateNewExercice(_maxChoices, _minRandomRange, _maxRandomRange);
+        Debug.Log(exerciceData.correctNumber);
+        _numberWord.text = _textManager.NumberToWords(exerciceData.correctNumber);
+
+        yield return ShowExerciceNumber();
+
+        yield return ChooseOptionRoutine(exerciceData);
+        
+        yield return End();
+
+        StartCoroutine(Play());
+    }
+    
+    private IEnumerator ShowExerciceNumber()
+    {
+        yield return _numberWord.DoAlphaTransition(1, _exerciceNumberAlphaDuration);
+        yield return new WaitForSeconds(_timeShowingExerciceNumber);
+        yield return _numberWord.DoAlphaTransition(0, _exerciceNumberAlphaDuration);
+    }
+    
+    private IEnumerator ChooseOptionRoutine(Exercice exerciceData)
+    {
+        _optionsController.StartExercice(exerciceData, _exerciceAnswer);
+        _exerciceAnswer.StartExerciceAnswer(exerciceData, _optionsController);
+
+        yield return new WaitUntil(() => _exerciceAnswer.HasEndExercice);
+        yield return new WaitForSeconds(_timeShowingAnswer);
+
+        yield return _optionsController.EndExercice();
+    }
+
+    public IEnumerator End()
+    {
+        yield return _optionsController.EndExercice();
+    }
+}
